03942b28b293d0aef29abaf89660fba69b2cb36a
Developers of Cspace <krishna.k@In.ey.com>
2020-08-29 18:02:18 +0530

    Initial commit for Dark and Light theme switcher

---
+++ src/renderer/components/TopBar/topBar.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <div>
+      <b-navbar toggleable="lg" :type="theme?'dark':'light'" :variant="theme?'dark':'light'">
+        <b-navbar-brand href="#">Redis Lion</b-navbar-brand>
+        <!-- Right aligned nav items -->
+        <b-navbar-nav class="ml-auto">
+          <b-nav-item-dropdown right>
+            <!-- Using 'button-content' slot -->
+            <template v-slot:button-content>
+              <em>Settings</em>
+            </template>
+            
+              <b-dropdown-item href="#">Sign Out</b-dropdown-item>
+              Dark Theme:<b-form-checkbox v-model="theme" name="check-button" switch>
+              </b-form-checkbox>
+     
+            
+          </b-nav-item-dropdown>
+        </b-navbar-nav>
+      </b-navbar>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      theme: false
+    }
+  }
+}
+</script>
